70646ad629440c7c0af7b1ded51d30cda4cb8c60
Lav Singh <lavkumar01@gmail.com>
2025-12-12 02:42:15 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,13 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-17-jdk -y
+# Installing Git
+sudo apt install git -y
+
+# Installing Maven
+sudo apt install maven -y
+
+git --version
+mvn --version
+java --version
+
